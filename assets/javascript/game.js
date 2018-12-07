7ee839d5ed4ea7158d6d514f1b75d0f92b2f7585
setheugene <seth.eugene@icloud.com>
2018-12-07 11:34:45 -0600

    style and html done

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+//on load assign amounts to crystals
+$(document).ready(function() {
+
+//for holding values
+    var crystal1 = "";
+    var crystal2 = "";
+    var crystal3 = "";
+    var crystal4 = "";
+
+//for adding score
+    var wins = 0;
+    var losses = 0;
+
+//for picking new values/ reseting added number
+    var reset = false;
+
+//pick and display goal number
+function reset() {
+    
+}
+
+//when a crystal is clicked it's value is added to #added
+
+//if added number is more than goal number then add one to losses
+
+//if added number equals goal number add one to wins and repick goal number and reset added number to 0
+)}
