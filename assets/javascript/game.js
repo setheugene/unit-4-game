f469bc02426a9d42baef0619818c5ad13830d4be
setheugene <seth.eugene@icloud.com>
2018-12-12 17:54:55 -0600

    done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,87 @@
-//on load assign amounts to crystals
+
 $(document).ready(function() {
 
-//for holding values
-    var crystal1 = "";
-    var crystal2 = "";
-    var crystal3 = "";
-    var crystal4 = "";
+        var score = 0
+        var goalNumber = 0
+        var wins = 0
+        var losses = 0 
+        var crystal1 = 0
+        var crystal2 = 0
+        var crystal3 = 0
+        var crystal4 = 0
 
-//for adding score
-    var wins = 0;
-    var losses = 0;
-
-//for picking new values/ reseting added number
-    var reset = false;
-
-//pick and display goal number
-function reset() {
+    var restart = function(){
+        crystal1 = Math.floor(Math.random() * 12) + 1;;
+        crystal2 = Math.floor(Math.random() * 12) + 1;;
+        crystal3 = Math.floor(Math.random() * 12) + 1;;
+        crystal4 = Math.floor(Math.random() * 12) + 1;;
     
-}
 
-//when a crystal is clicked it's value is added to #added
+    console.log (crystal1);
+    console.log (crystal2);
+    console.log (crystal3);
+    console.log (crystal4);
 
-//if added number is more than goal number then add one to losses
+    goalNumber = Math.floor(Math.random() * (120-19) + 19);
+    $("#goal-number").text(goalNumber)
 
-//if added number equals goal number add one to wins and repick goal number and reset added number to 0
-)}
+    console.log (goalNumber);
+
+    score = 0
+    $("#score").text("Your total score is: " + score);
+    };
+
+    $("#blue").append("<img src='assets/images/blue.png' width='100px'/>");
+    $("#green").append("<img src='assets/images/green.jpg' width='100px'/>");
+    $("#red").append("<img src='assets/images/red.png' width='100px'/>");
+    $("#yellow").append("<img src='assets/images/yellow.png' width='100px'/>");
+
+
+    restart();
+
+    $("#blue").click(function(){
+        score = score + crystal1;
+    $("#score").text(" " + score);
+    add()
+    console.log(this);
+    });
+
+    $("#green").click(function(){
+        score = score + crystal2;
+    $("#score").text(" " + score);
+    add()
+    console.log(this);
+    });
+
+    $("#red").click(function(){
+        score = score + crystal3;
+    $("#score").text(" " + score);
+    add()
+    console.log(this);
+    });
+
+    $("#yellow").click(function(){
+        score = score + crystal4;
+    $("#score").text(" " + score);
+    add()
+    console.log(this);
+    });
+    
+
+    var add = function(){
+        if (score > goalNumber){
+            alert("You lost...");
+            losses = losses + 1
+            $("#losses").text("Losses: " + losses);
+            restart();
+        }
+
+        if (score === goalNumber){
+            alert("You Win!");
+            wins = wins + 1
+            $("#wins").text("Wins: " + wins);
+            restart();
+        }
+    };
+
+})
